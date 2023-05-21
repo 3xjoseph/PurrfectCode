7177629a3cd9ad28778c644ee2fa3928e586bb7b
Patrick Pronuevo <128480468+3xjoseph@users.noreply.github.com>
2023-05-21 20:15:16 +0800

    book animation done

--- JavaHub/Assets/script/bookSCript.cs
+++ JavaHub/Assets/script/bookSCript.cs
@@ -4,22 +4,55 @@ using UnityEngine;
 
 public class bookScript : MonoBehaviour
 {
-
+    
     [SerializeField] float pageSpeed = 0.5f;
     [SerializeField] List<Transform> pages;
+    [SerializeField] GameObject backButton;
+    [SerializeField] GameObject forwardButton;
     int index = -1;
+    bool rotate = false;
+
 
     public void RotateForward()
     {
+        if (rotate == true) { return; }
         index++;
         float angle = 180;
+        
+        pages[index].SetAsLastSibling();
         StartCoroutine(Rotate(angle, true));
+    }
+
+    public void ForwardButtonActions()
+    {
+        if (backButton.activeInHierarchy == false)
+        {
+            backButton.SetActive(true); //every time we turn the page forward, the back button should be activated
+        }
+        if (index == pages.Count - 1)
+        {
+            forwardButton.SetActive(false); //if the page is last then we turn off the forward button
+        }
     }
 
     public void RotateBack()
     {
+        if (rotate == true){ return; }
         float angle = 0;
+        
         StartCoroutine(Rotate(angle, false));
+    }
+
+    public void BackButtonActions()
+    {
+        if (forwardButton.activeInHierarchy == false)
+        {
+            forwardButton.SetActive(true); //every time we turn the page back, the forward button should be activated
+        }
+        if (index - 1 == -1)
+        {
+            backButton.SetActive(false); //if the page is first then we turn off the back button
+        }
     }
 
     IEnumerator Rotate(float angle, bool forward)
@@ -27,6 +60,7 @@ public class bookScript : MonoBehaviour
         float value = 0f;
         while (true)
         {
+            rotate = true;
             Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
             value += Time.deltaTime * pageSpeed;
             pages[index].rotation = Quaternion.Slerp(pages[index].rotation, targetRotation, value);
@@ -37,6 +71,7 @@ public class bookScript : MonoBehaviour
                 {
                     index++;
                 }
+                rotate = false;
                 break;
             }
             yield return null;
