bca31d9f4280872a1e7cf4abcf5fa8a188844292
Patrick Pronuevo <128480468+3xjoseph@users.noreply.github.com>
2023-05-21 18:32:03 +0800

    update script book

--- JavaHub/Assets/script/bookSCript.cs
+++ JavaHub/Assets/script/bookSCript.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class bookSCript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float pageSpeed = 0.5f;
+    [SerializeField] List<Transform> pages;
+    int index = -1;
+
+    public void RotateForward()
     {
-        
+        index++;
+        float angle = 180;
+        StartCoroutine(Rotate(angle, true));
     }
 
-    // Update is called once per frame
-    void Update()
+    public void RotateBack()
     {
-        
+        float angle = 0;
+        StartCoroutine(Rotate(angle, false));
+    }
+
+    IEnumerator Rotate(float angle, bool forward)
+    {
+        float value = 0f; 
+        while(true)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, angle, 0);
+            value += Time.deltaTime * pageSpeed;
+            pages[index].rotation = Quaternion.Slerp(pages[index].rotation, targetRotation, value);
+            float angle1 = Quaternion.Angle(pages[index].rotation, targetRotation);
+            if(angle1 < 0.1f)
+            {
+                if (forward == false)
+                {
+                    index++;
+                }
+                break;
+            }
+            yield return null;
+        }
     }
 }
