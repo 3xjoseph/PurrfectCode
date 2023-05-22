706ec3d49f0120ec27381e319e60ac7492e48636
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-22 20:15:56 +0800

    Level 1
    Finishing backend and animation part for level 1 with different questions

--- JavaHub/Assets/checkAnswer.cs
+++ JavaHub/Assets/checkAnswer.cs
@@ -1,28 +1,138 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class checkAnswer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject enemyHeart;
+    public GameObject catHeart;
+    public GameObject popUpDialog;
+    public Text question, textA,textB,textC,textD;
 
+    public GameObject snakeObj;
+    public GameObject catObj;
+    
+
+    int index = 0;
+    String[] answer = { "choiceC", "choiceC", "choiceD", "choiceA", "choiceD", "choiceA", "choiceA", "choiceD", "choiceB", "choiceC" };
+    String[] questionnaire = {
+        "How are statements terminated in Java?",
+        "How do you display output in Java?",
+        "What are the two types of comments in Java?",
+        "How do you declare a variable in Java?",
+        "What is a data type in Java?",
+        "What is the difference between int and double data types in Java?",
+        "How do you assign a value to a variable in Java?",
+        "What is the default value of an uninitialized variable in Java?",
+        "How do you convert a string to an integer in Java?"
+    };
+
+    String[] choiceA =
+    {
+        "a) Colon (:)", "a) print()" , "a)	Single-line and multi-line comments" ,
+        "a)	define","a)	A type of variable","a)	int is used for whole numbers, while double is used for decimal numbers",
+        "a)	assign()","a) null", "a) Integer.toInteger()"
+
+    };
+
+    String[] choiceB =
+    {
+        "b)	Comma (,)", "b)	display()", "b)	Small and big comments", "b) var", "b)	A type of class",
+        "b)	int is used for decimal numbers, while double is used for whole numbers", "b) store()",
+        "b)	0","b)	String.toInteger()",
+
+    };
+    String[] choiceC =
+    {
+        "c)	Semicolon (;)", "c)	show()", "c) Red and blue comments", "c) set", "c)	A type of loop",
+        "c)	int is used for text strings, while double is used for numbers","c)	set()", "c)	undefined",
+        "c)	Integer.parseInt()"
+    };
+
+    String[] choiceD =
+    {
+        "d)	Period (.)", "d) System.out.println()", "d)	Main and sub comments", "d)	type", "d)	A type of function",
+        "d)	int and double are the same data type", "d)	= (equals sign)", "d) void", "d) String.parseInt()"
+    };
+
+    private void Start()
+    {
+       
+    }
     public void click()
     {
-        string buttonName = EventSystem.current.currentSelectedGameObject.name;
-        if (buttonName=="choiceC")
-            Debug.Log("your answer is correct "+ buttonName);
+        string buttonName = EventSystem.current.currentSelectedGameObject.name; //get the name of the button has been clicked
+        Answer(buttonName);
+ 
+    }
+
+    private void Answer(String buttonName)
+    {
+        //correct answer
+        if (buttonName == answer[index])
+        {
+            catScript cat = catObj.GetComponent<catScript>();
+            cat.Move();
+            //while the question is answered correctly it will go to the next index of the array of questions
+            RemoveHeart(enemyHeart); //reduce the heart of enemy
+
+            //change the questions
+            StartCoroutine(waitToProceed());
+            index++;
+
+        }
+        //wrong answer
         else
-            Debug.Log("your answer is incorrect " + buttonName);
+        {
+            snakeScript snake = snakeObj.GetComponent<snakeScript>();
+            snake.Move();
+            RemoveHeart(catHeart); //reduce the heart of cat
+        }
+            
+    }
+
+    private void proceed(String questions)
+    {
+        question.text = questions; //change the question
+        String forA = choiceA[index];
+        String forB = choiceB[index];
+        String forC = choiceC[index];
+        String forD = choiceD[index];
+        changeChoices(forA, forB, forC, forD);
+    }
+
+    private void changeChoices(string forA, string forB, string forC, string forD)
+    {
+        // textA,textB,textC,textD
+        textA.text = forA;
+        textB.text = forB;
+        textC.text = forC;
+        textD.text = forD;
+
+    }
+
+
+    //remove a heart from the user/enemy 
+    public void RemoveHeart(GameObject heart)
+    {
+        if (heart.transform.childCount > 0)
+        {
+            Transform lastHeart = heart.transform.GetChild(heart.transform.childCount - 1);
+            Destroy(lastHeart.gameObject);
+        }
+        else
+            popUpDialog.SetActive(true); //if there's no heart then you cannot proceed/play
+    }
+
+    IEnumerator waitToProceed()
+    {
+        yield return new WaitForSeconds(2f);
+        proceed(questionnaire[index]);
     }
 }
